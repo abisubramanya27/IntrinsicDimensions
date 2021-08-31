673243c9d4956f558f066f991d65c9fffa2ffd71
abisubramanya27 <abisubramanya27@gmail.com>
2021-08-31 17:25:17 +0530

    added fastfood_fly.py and reorganized fb_id_test dir

--- id_fb_test/fwh_extension/fwh.py
+++ id_fb_test/fwh_extension/fwh.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='fwh',
+    name='fwh_cuda',
     ext_modules=[
         CUDAExtension('fwh_cuda', [
             'fwh_cpp.cpp',
